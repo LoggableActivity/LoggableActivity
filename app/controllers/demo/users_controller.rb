63444838bfa7c3cc7f06c45c361b1d4e7330287a
Max Groenlund <mg@opentelehealth.com>
2024-01-05 12:49:41 +0100

    Updated User with name, and bio

---
+++ app/controllers/demo/users_controller.rb
@@ -0,0 +1,48 @@
+class Demo::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to demo_users_path, notice: 'User was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to demo_users_path, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to demo_users_url, notice: 'User was successfully destroyed.'
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  namespace :demo do
+    resources :users
+  end
 end
