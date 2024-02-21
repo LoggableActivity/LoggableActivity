5b6d43ede18dc9ed699adeb7c7d8b7a6c19d4ca9
Max Groenlund <max@synthmax.dk>
2024-02-21 21:19:39 +0100

    Added suport for has_many relations

--- spec/loggable_activity/hooks_spec.rb
+++ spec/loggable_activity/hooks_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe LoggableActivity::Hooks, type: :model do
   describe 'activities' do
     before do
       stub_const('Rails', double('Rails'))
-      allow(Rails).to receive_message_chain(:application, :config, :loggable_activity).and_return(Hash.new(actor_display_name: :full_name, current_user_model_name: 'User'))
+      allow(Rails).to receive_message_chain(:application, :config, :loggable_activity).and_return(OpenStruct.new(actor_display_name: :full_name, current_user_model_name: 'User'))
       current_user = MockUser.create(name: 'John Doe', email: 'john.doe@example.com')
       Thread.current[:current_user] = current_user
     end
