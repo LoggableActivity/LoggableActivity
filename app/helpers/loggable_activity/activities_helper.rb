509ec2281680cd8ed54fb01bdcc902b33875e2cf
Max Groenlund <max@synthmax.dk>
2024-07-07 19:22:24 +0200

    Add helper to display payload

--- app/helpers/loggable_activity/activities_helper.rb
+++ app/helpers/loggable_activity/activities_helper.rb
@@ -3,8 +3,16 @@
 module LoggableActivity
   # Helper methods for activities
   module ActivitiesHelper
+    require 'json'
     def activity_action(activity)
       I18n.t("loggable_activity.#{activity.action}")
+    end
+
+  
+    def format_json_for_display(hash_data)
+      JSON.pretty_generate(hash_data)
+    rescue JSON::ParserError
+      hash_data.to_s
     end
   end
 end
