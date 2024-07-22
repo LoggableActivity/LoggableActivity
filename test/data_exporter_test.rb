0f2dcbb002c1bd68ac1c1f2d64d8b1ca5ae7a5e7
Max Groenlund <max@synthmax.dk>
2024-07-22 13:21:59 +0200

    Feature: Download as CVS
    #75

---
+++ test/data_exporter_test.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module LoggableActivity
+  class DataExporterTest < ActiveSupport::TestCase
+    setup do
+      Thread.current[:current_actor] = @current_user = create(:user, user_type: 'admin')
+      # @company = Company.create(name: 'Company')
+    end
+
+    test 'export_csv returns a CSV file' do
+      record =
+        User.create(
+          first_name: 'John',
+          last_name: 'Doe',
+          email: 'john-doe@example.com',
+          password: 'password',
+          age: 30,
+          user_type: 'admin',
+          company: @current_user.company
+        )
+      record.update(first_name: 'Jane')
+      activity = LoggableActivity::Activity.last
+      activity.update(created_at: 6.seconds.ago)
+      record.log(:show)
+      Hat.create(color: 'Cyan', user: record)
+      record.reload.hats
+      record.destroy
+      exporter = LoggableActivity::DataExporter.new(record)
+      csv_data = exporter.export_csv
+      file_path = '/Users/maxgronlund/Documents/Max/open-source/LoggableActivityEngine/LoggableActivityEngine/file.csv'
+      File.write(file_path, csv_data)
+    end
+  end
+end
