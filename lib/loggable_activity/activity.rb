4f6c135b5dbb0547824f0fa09a44900c7344c8f3
Bhalani <hirenbhalani777@gmail.com>
2024-09-01 13:07:11 +0200

    Remove redundant public attributes

--- lib/loggable_activity/activity.rb
+++ lib/loggable_activity/activity.rb
@@ -80,8 +80,7 @@ module LoggableActivity
           route: payload.payload_route,
           record_display_name: payload.fetch_record_display_name,
           current_payload: payload.current_payload,
-          data_owner: payload.data_owner,
-          public_attrs: payload.public_attrs
+          data_owner: payload.data_owner
         }
       end
     end
